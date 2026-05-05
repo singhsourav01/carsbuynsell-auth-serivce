db0c806adf204881da9aa87daf8de690bb4fea16
Sourav Singh <singhsourav0110@gmail.com>
2026-05-05 21:20:56 +0530

    fix: update USER_SERVICE_URL to correct endpoint

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { handleAxiosError } from "../utils/helper";
 
-const USER_SERVICE_URL = process.env.USER_SERVICE_URL || "http://13.127.188.130:3002/user";
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL || "http://13.235.103.86:3002/user";
 
 export const createUser = async (userData: any, token?: string) => {
   const { data } = await axios.post(
